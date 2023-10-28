073304e33ad06228cccefea73026fdd90ac29430
shubhanshu1902 <shubhanshu1902@gmail.com>
2023-10-28 22:31:41 +0530

    trying to resolve error 2

--- src/main/java/com/mycompany/app/Calculator.java
+++ src/main/java/com/mycompany/app/Calculator.java
@@ -2,76 +2,76 @@ package com.mycompany.app;
 
 public class Calculator
 {
-	Double computeSquareRoot(Double inpX)
-	{
-                if(inpX < 0)
-                {
-                        System.out.println("Negative number, really? Exiting...");
-			return null;
-                }
-                else
-                {
-                        System.out.print("The square root is: ");
-                        Double res = Math.sqrt(inpX);
-			System.out.println(res);
-			return res;
-                }
-	}
+	// Double computeSquareRoot(Double inpX)
+	// {
+    //             if(inpX < 0)
+    //             {
+    //                     System.out.println("Negative number, really? Exiting...");
+	// 		return null;
+    //             }
+    //             else
+    //             {
+    //                     System.out.print("The square root is: ");
+    //                     Double res = Math.sqrt(inpX);
+	// 		System.out.println(res);
+	// 		return res;
+    //             }
+	// }
 
-	Integer computeFactorial(Integer inpX)
-	{
-		if(inpX < 0)
-                {
-                        System.out.println("Negative number, seriously? Exiting...");
-                        return null; 
-                }
-                else
-                {
-                        Integer res = 1;
-                        for(int i = 1; i <= inpX; i++)
-                        {
-                                res = res * i;
-                        }
-                        System.out.print("The factorial is: ");
-                        System.out.println(res);
-			return res; 
-                }
-	}
+	// Integer computeFactorial(Integer inpX)
+	// {
+	// 	if(inpX < 0)
+    //             {
+    //                     System.out.println("Negative number, seriously? Exiting...");
+    //                     return null; 
+    //             }
+    //             else
+    //             {
+    //                     Integer res = 1;
+    //                     for(int i = 1; i <= inpX; i++)
+    //                     {
+    //                             res = res * i;
+    //                     }
+    //                     System.out.print("The factorial is: ");
+    //                     System.out.println(res);
+	// 		return res; 
+    //             }
+	// }
 
-	Double computeNaturalLog(Double inpX)
-	{
-		if(inpX < 0)
-                {
-                        System.out.println("Well, natural log of negative numbers is not defined!, Exiting...");
-                	return null;
-		}
-                else if(inpX == 0.0)
-                {
-                        System.out.println("Natural log of 0 is not defined! Exiting...");
-                	return null; 
-		}
-                else
-                {
-                        Double res = Math.log(inpX);
-                        System.out.print("The natural log is: ");
-                        System.out.println(res);
-			return res; 
-                }
-	}
+	// Double computeNaturalLog(Double inpX)
+	// {
+	// 	if(inpX < 0)
+    //             {
+    //                     System.out.println("Well, natural log of negative numbers is not defined!, Exiting...");
+    //             	return null;
+	// 	}
+    //             else if(inpX == 0.0)
+    //             {
+    //                     System.out.println("Natural log of 0 is not defined! Exiting...");
+    //             	return null; 
+	// 	}
+    //             else
+    //             {
+    //                     Double res = Math.log(inpX);
+    //                     System.out.print("The natural log is: ");
+    //                     System.out.println(res);
+	// 		return res; 
+    //             }
+	// }
 
-	Double computePower(Double base, Double power)
-	{
-		Double res = Math.pow(base, power);
-		if(Double.isNaN(res))
-		{
-			System.out.println("Error, cannot compute power");
-			return null;
-		}
-		else
-		{
-			System.out.print("The result is: ");
-			System.out.println(res);
-			return res; 
-		}
-	}
+	// Double computePower(Double base, Double power)
+	// {
+	// 	Double res = Math.pow(base, power);
+	// 	if(Double.isNaN(res))
+	// 	{
+	// 		System.out.println("Error, cannot compute power");
+	// 		return null;
+	// 	}
+	// 	else
+	// 	{
+	// 		System.out.print("The result is: ");
+	// 		System.out.println(res);
+	// 		return res; 
+	// 	}
+	// }
 }
