caf1450e5d79ac970ab13cf7a43af1fbe9e897d3
shubhanshu1902 <shubhanshu1902@gmail.com>
2023-10-31 00:30:24 +0530

    calculator done

---
+++ src/main/java/calculator/Main.java
@@ -0,0 +1,53 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("Welcome to Scientific Calculator");
+            System.out.println("What would you like to do?");
+            System.out.println("1. Enter 1 to compute Addition");
+            System.out.println("2. Enter 2 to compute Subtraction");
+            System.out.println("3. Enter 3 to compute Multiplication");
+            System.out.println("4. Enter 4 to compute Division");
+            System.out.println("5. Enter 5 to exit the program");
+            System.out.print("Enter your choice: ");
+            Scanner sc = new Scanner(System.in);
+            int choice = sc.nextInt();
+
+            Calculator calcObj = new Calculator();
+
+            if (choice >= 5) {
+                System.out.println("Exiting program...");
+                System.exit(0);
+            } else if (choice == 1) {
+                System.out.print("Enter the First number: ");
+                Double a = sc.nextDouble();
+                System.out.print("Enter the Second number: ");
+                Double b = sc.nextDouble();
+                calcObj.add(a, b);
+            } else if (choice == 2) {
+                System.out.print("Enter the First number: ");
+                Double a = sc.nextDouble();
+                System.out.print("Enter the Second number: ");
+                Double b = sc.nextDouble();
+                calcObj.sub(a, b);
+
+            } else if (choice == 3) {
+                System.out.print("Enter the First number: ");
+                Double a = sc.nextDouble();
+                System.out.print("Enter the Second number: ");
+                Double b = sc.nextDouble();
+                calcObj.mul(a, b);
+
+            } else {
+                System.out.print("Enter the Dividend: ");
+                Double a = sc.nextDouble();
+                System.out.print("Enter the Divisor: ");
+                Double b = sc.nextDouble();
+                calcObj.div(a, b);
+            }
+        }
+    }
+}
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,75 @@
+package calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+    Calculator calcObj = new Calculator();
+
+    @Test
+    public void testAdd()
+	{
+		// Test 1
+		assertEquals((Double) 4.0, calcObj.add(1.0,3.0));
+
+		// Test 2
+		assertEquals((Double) 0.0, calcObj.add(0.0,0.0));
+
+		// Test 3
+		assertEquals((Double) 9.525, calcObj.add(9.5,0.025));
+
+		// Test 4
+		assertEquals((Double) 1.0, calcObj.add(2.0,-1.0));
+	}
+    @Test
+    public void testSub()
+	{
+		// Test 1
+		assertEquals((Double) 4.0, calcObj.sub(7.0,3.0));
+
+		// Test 2
+		assertEquals((Double) 0.0, calcObj.sub(2.0,2.0));
+
+		// Test 3
+        Double ts = -9.525;
+		assertEquals(ts, calcObj.sub(-9.5,0.025));
+
+		// Test 4
+		assertEquals((Double) 1.0, calcObj.sub(2.0,1.0));
+	}
+
+    @Test
+    public void testMul()
+	{
+		// Test 1
+		assertEquals((Double) 4.0, calcObj.mul(1.0,4.0));
+
+		// Test 2
+		assertEquals((Double) 0.0, calcObj.mul(10.0,0.0));
+
+		// Test 3
+        Double ts = -9.3;
+		assertEquals(ts, calcObj.mul(-3.1,3.0));
+
+		// Test 4
+		assertEquals((Double) 1.0, calcObj.mul(-1.0,-1.0));
+	}
+    
+    @Test
+    public void testDiv()
+	{
+		// Test 1
+		assertEquals((Double) 4.0, calcObj.div(4.0,1.0));
+
+		// Test 2
+		assertEquals((Double) 0.0, calcObj.div(0.0,10.0));
+
+		// Test 3
+        Double ts = -3.1;
+		assertEquals(ts, calcObj.div(-9.3,3.0));
+
+		// Test 4
+		assertNull(calcObj.div(19.0, 0.0));
+	}
+}
